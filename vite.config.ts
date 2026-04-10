55a40583c677a1f358e8a3b2f0523b25081291cd
cheesequake <dev.cheesequake@gmail.com>
2026-04-10 19:00:56 +0530

    add base path

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/dmrc-demo/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
@@ -18,7 +19,6 @@ export default defineConfig(({ mode }) => {
     server: {
       hmr: process.env.DISABLE_HMR !== 'true',
 
-      // ✅ Add this
       allowedHosts: [
         'syntrackpro.com',
         'www.syntrackpro.com'
